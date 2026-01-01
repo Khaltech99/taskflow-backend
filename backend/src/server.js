bfe675d2ddef26f85ad0fd8ba1e1e2827c1b418d
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-01 23:14:28 +0100

    style: Format code for consistency in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,7 @@
 import app from "./app.js";
 
-const port=process.env.PORT;
+const port = process.env.PORT;
 
-app.listen(port,()=>{
-    console.log(`Running at port:${port}`);
-    
-})
+app.listen(port, () => {
+  console.log(`Running at port:${port}`);
+});
