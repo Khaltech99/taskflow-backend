e8c585fc0ab36b68a9e3fc4884a72bad22859114
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-01 23:14:43 +0100

    refactor: Clean up registerController by removing console logs and uncommenting request body extraction

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,7 @@
 import { catchAsync } from "../utils/catchAsync.js";
 
 export const registerController = catchAsync(async (req, res, next) => {
-  console.log("hey");
-  res.send("away");
-  //const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 });
 
 export const loginController = async (req, res, next) => {
