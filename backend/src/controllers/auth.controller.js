8e660bde592828ba3dae9127f74914c59e71e5cb
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-02 01:21:09 +0100

    Feat: Added rgister and login functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,16 @@
+import { loginService, registerService } from "../service/auth.service.js";
 import { catchAsync } from "../utils/catchAsync.js";
 
 export const registerController = catchAsync(async (req, res, next) => {
   const { name, email, password } = req.body;
+  const user = await registerService({ name, email, password });
+  res.status(201).json({ message: "User registered successfully", user });
+  next();
 });
 
-export const loginController = async (req, res, next) => {
-  res.send("login");
-};
+export const loginController = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await loginService({ email, password });
+  res.status(200).json({ message: "User logged in successfully", user });
+  next();
+});
