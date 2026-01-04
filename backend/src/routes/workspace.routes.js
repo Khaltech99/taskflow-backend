c8096f9b8d3124a6ef5947a98181e1da1a5b461d
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-04 23:02:39 +0100

    Feat: implement the createworspace and getworspace functionality

--- backend/src/routes/workspace.routes.js
+++ backend/src/routes/workspace.routes.js
@@ -1,9 +1,13 @@
 import Router from "express";
-import { createWorkspace } from "../controllers/workspace.controller.js";
+import {
+  createWorkspace,
+  getMyWorkspaces,
+} from "../controllers/workspace.controller.js";
 import { protectedRoute } from "../middleware/protectedRoute.js";
 
 const workspaceRouter = Router();
 
 workspaceRouter.post("/workspace", protectedRoute, createWorkspace);
+workspaceRouter.get("/workspace", protectedRoute, getMyWorkspaces);
 
 export default workspaceRouter;
