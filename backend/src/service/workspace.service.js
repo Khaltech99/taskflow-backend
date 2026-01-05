b3a70abe2394070269ecee82cd17e7f88d10da1b
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-05 21:21:09 +0100

    feat: Implement workspace creation, retrieval, and member management services.

--- backend/src/service/workspace.service.js
+++ backend/src/service/workspace.service.js
@@ -1,3 +1,4 @@
+import { users } from "../models/user.model.js";
 import { workspaces } from "../models/workspace.model.js";
 import { error } from "../utils/error.js";
 
@@ -41,4 +42,40 @@ export const getMyWorkSpaceServices = async ({ userId }) => {
     .sort({ createdAt: -1 });
 
   return userWorkspaces;
+};
+
+export const addMemberService = async ({ workspaceId, userId }) => {
+  if (!workspaceId || !userId) {
+    throw error(400, "Workspace id and user id are required");
+  }
+
+  // check if the user exists
+  const user = await users.findById(userId);
+  if (!user) {
+    throw error(404, "User not found");
+  }
+
+  const workspace = await workspaces.findById(workspaceId);
+
+  // check if workspace exists
+
+  if (!workspace) {
+    throw error(404, "Workspace not found");
+  }
+
+  // check if the user is the owner
+  if (workspace.owner.toString() !== userId) {
+    throw error(403, "You are not authorized to add members to this workspace");
+  }
+  // check if the user is already a member
+  if (workspace.members.includes(userId)) {
+    throw error(409, "User is already a member of this workspace");
+  }
+
+  // add the user to the workspace
+  workspace.members.push(userId);
+
+  await workspace.save();
+
+  return workspace;
 };
