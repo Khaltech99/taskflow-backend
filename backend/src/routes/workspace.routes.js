841c0304ec3482558ea33a82e7686fda22b90e59
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-05 21:20:54 +0100

    feat: Add workspace creation, retrieval, and member management functionality.

--- backend/src/routes/workspace.routes.js
+++ backend/src/routes/workspace.routes.js
@@ -1,5 +1,6 @@
 import Router from "express";
 import {
+  addMembers,
   createWorkspace,
   getMyWorkspaces,
 } from "../controllers/workspace.controller.js";
@@ -9,5 +10,6 @@ const workspaceRouter = Router();
 
 workspaceRouter.post("/workspace", protectedRoute, createWorkspace);
 workspaceRouter.get("/workspace", protectedRoute, getMyWorkspaces);
+workspaceRouter.post("/workspace/:workspaceId", protectedRoute, addMembers);
 
 export default workspaceRouter;
