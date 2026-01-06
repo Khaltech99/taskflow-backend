47e7b38d9b57c50557d791ce2b8343ebb9214880
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-06 18:06:50 +0100

    Add: add workspace router.

--- backend/src/routes/workspace.routes.js
+++ backend/src/routes/workspace.routes.js
@@ -2,6 +2,7 @@ import Router from "express";
 import {
   addMembers,
   createWorkspace,
+  deleteWorkspace,
   getMyWorkspaces,
 } from "../controllers/workspace.controller.js";
 import { protectedRoute } from "../middleware/protectedRoute.js";
@@ -11,5 +12,10 @@ const workspaceRouter = Router();
 workspaceRouter.post("/workspace", protectedRoute, createWorkspace);
 workspaceRouter.get("/workspace", protectedRoute, getMyWorkspaces);
 workspaceRouter.post("/workspace/:workspaceId", protectedRoute, addMembers);
+workspaceRouter.delete(
+  "/workspace/:workspaceId",
+  protectedRoute,
+  deleteWorkspace
+);
 
 export default workspaceRouter;
