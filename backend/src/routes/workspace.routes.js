766f3de2ac527e1e99755bdc7482d947a0610924
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-07 21:59:11 +0100

    Feat: use the editworspace controller in the route

--- backend/src/routes/workspace.routes.js
+++ backend/src/routes/workspace.routes.js
@@ -3,19 +3,31 @@ import {
   addMembers,
   createWorkspace,
   deleteWorkspace,
+  editWorkspace,
   getMyWorkspaces,
 } from "../controllers/workspace.controller.js";
 import { protectedRoute } from "../middleware/protectedRoute.js";
 
 const workspaceRouter = Router();
 
+// create workspace
 workspaceRouter.post("/workspace", protectedRoute, createWorkspace);
+
+// get workspace
 workspaceRouter.get("/workspace", protectedRoute, getMyWorkspaces);
+
+// add members
 workspaceRouter.post("/workspace/:workspaceId", protectedRoute, addMembers);
+
+// delete workspace
 workspaceRouter.delete(
   "/workspace/:workspaceId",
   protectedRoute,
   deleteWorkspace
 );
+
+// edit workspace
+
+workspaceRouter.patch("/workspace/:workspaceId", protectedRoute, editWorkspace);
 
 export default workspaceRouter;
