07667aa6e54460ffe644886e9209615475e082ec
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-08 23:49:09 +0100

    Feat: Implement project delete functionality on individual workspace

--- backend/src/controllers/project.controller.js
+++ backend/src/controllers/project.controller.js
@@ -1,5 +1,6 @@
 import {
   createProjectService,
+  deleteProjectServices,
   getProjectService,
 } from "../service/project.services.js";
 import { catchAsync } from "../utils/catchAsync.js";
@@ -33,4 +34,16 @@ export const getProject = catchAsync(async (req, res) => {
   const projects = await getProjectService({ workspaceId, userId });
 
   res.status(200).json({ success: true, data: projects });
+});
+
+export const deleteProject = catchAsync(async (req, res) => {
+  const { workspaceId, projectId } = req.params;
+  const userId = req.user.sub.id;
+  const project = await deleteProjectServices({
+    workspaceId,
+    projectId,
+    userId,
+  });
+
+  res.status(200).json({ success: true, data: project });
 });
