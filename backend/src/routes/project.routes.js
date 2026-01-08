ec847b1b4488298efc2df1958355b4b0e85c54f5
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-08 23:49:36 +0100

    Add: Add the deleteProject route.

--- backend/src/routes/project.routes.js
+++ backend/src/routes/project.routes.js
@@ -1,6 +1,7 @@
 import Router from "express";
 import {
   createProject,
+  deleteProject,
   getProject,
 } from "../controllers/project.controller.js";
 import { protectedRoute } from "../middleware/protectedRoute.js";
@@ -9,5 +10,10 @@ const projectRouter = Router();
 
 projectRouter.post("/projects/:workspaceId", protectedRoute, createProject);
 projectRouter.get("/projects/:workspaceId", protectedRoute, getProject);
+projectRouter.delete(
+  "/projects/:workspaceId/:projectId",
+  protectedRoute,
+  deleteProject
+);
 
 export default projectRouter;
