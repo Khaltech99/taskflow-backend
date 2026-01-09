68a7f13f4bddab6e500e93e8db9a32757acc87aa
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-09 21:18:50 +0100

    Add: Add task route

--- backend/src/routes/project.routes.js
+++ backend/src/routes/project.routes.js
@@ -8,8 +8,13 @@ import { protectedRoute } from "../middleware/protectedRoute.js";
 
 const projectRouter = Router();
 
+// create route
 projectRouter.post("/projects/:workspaceId", protectedRoute, createProject);
+
+// get route
 projectRouter.get("/projects/:workspaceId", protectedRoute, getProject);
+
+// delete route
 projectRouter.delete(
   "/projects/:workspaceId/:projectId",
   protectedRoute,
