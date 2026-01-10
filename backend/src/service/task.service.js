98fae3f90c85022cbc9ea075b7b1a1681ad2a1e1
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-10 22:28:19 +0100

    Optional chaining and bug fix.

--- backend/src/service/task.service.js
+++ backend/src/service/task.service.js
@@ -1,5 +1,6 @@
 import { projects } from "../models/project.model.js";
 import tasks from "../models/task.model.js";
+import { error } from "../utils/error.js";
 
 // create task service
 export const createTaskService = async ({
@@ -8,22 +9,33 @@ export const createTaskService = async ({
   memberId,
   taskData,
 }) => {
-  if (!userId || !projectId || !taskData) {
+  if (!userId || !projectId || !taskData || !memberId) {
     throw error(400, "Invalid data");
   }
 
   // check if the project exists
   const project = await projects.findById(projectId);
+  console.log(project);
+
   if (!project) {
     throw error(404, "Project not found");
   }
+  // check if it is the project owner
 
+  if (project?.owner.toString() !== userId) {
+    throw error(401, "User not authorized for this action");
+  }
   //   check if the the member trying to create is actually a member in the project
-  if (!project.members.includes(memberId)) {
+  if (!project?.members?.includes(memberId)) {
     throw error(404, "You are not a member in this project");
   }
 
-  const task = await tasks.create(taskData);
-
+  const task = await tasks.create({
+    ...taskData,
+    owner: userId,
+    projectId,
+    memberId,
+    assignedTo: memberId,
+  });
   return task;
 };
