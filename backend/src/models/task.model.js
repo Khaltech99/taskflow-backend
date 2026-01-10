8ad01f26d6cc932dcb611cd4f9d179d130b97484
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-10 22:26:04 +0100

    Task model Improvement.

--- backend/src/models/task.model.js
+++ backend/src/models/task.model.js
@@ -1,45 +1,48 @@
 import mongoose from "mongoose";
 
-const TaskSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  status: {
-    type: String,
-    default: "todo",
-    enum: ["todo", "in progress", "done"],
-    required: true,
-  },
+const TaskSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      default: "todo",
+      enum: ["todo", "in progress", "done"],
+      required: true,
+    },
 
-  priority: {
-    type: String,
-    default: "low",
-    enum: ["low", "medium", "high"],
-    required: true,
+    priority: {
+      type: String,
+      default: "low",
+      enum: ["low", "medium", "high"],
+      required: true,
+    },
+    assignedTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    projectId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  assignedTo: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  projectId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Project",
-    required: true,
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const tasks = mongoose.model("Task", TaskSchema);
 
