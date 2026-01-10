5a2ba1d33d40fe357935daaa84a7f112e3da5002
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-10 22:27:26 +0100

    Add: add protected route to the tasks route.

--- backend/src/routes/task.route.js
+++ backend/src/routes/task.route.js
@@ -1,8 +1,9 @@
 import Router from "express";
 import { createTask } from "../controllers/task.controller.js";
+import { protectedRoute } from "../middleware/protectedRoute.js";
 
 const taskRouter = Router();
 
 // create task
-taskRouter.post("/tasks", createTask);
+taskRouter.post("/tasks", protectedRoute, createTask);
 export default taskRouter;
