d63addc8805df1d87de4c16b0717621925fdb808
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-10 22:25:32 +0100

    Feat: Incoporate create task srvice into the controller

--- backend/src/controllers/task.controller.js
+++ backend/src/controllers/task.controller.js
@@ -3,8 +3,26 @@ import { catchAsync } from "../utils/catchAsync.js";
 
 // create task controller
 export const createTask = catchAsync(async (req, res) => {
-  const { userId } = req.user.sub.id;
-  const { name, description, status } = req.body;
+  const userId = req.user.sub.id;
+  const { name, description, owner, projectId, memberId } = req.body;
+  const { status, priority } = req.params;
 
-  const task = await createTaskService({});
+  const taskData = {
+    name,
+    description,
+    owner,
+    status,
+    priority,
+  };
+  const task = await createTaskService({
+    userId,
+    projectId,
+    memberId,
+    taskData,
+  });
+
+  res.status(201).json({
+    status: "success",
+    data: task,
+  });
 });
