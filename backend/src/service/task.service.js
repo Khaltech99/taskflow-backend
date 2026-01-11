32554d9caebf4631371c70bd5a8ba942bb82bc3a
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-11 13:54:29 +0100

    Feat: Remove logs and implement the gettask and the update task service with the former including pagination

--- backend/src/service/task.service.js
+++ backend/src/service/task.service.js
@@ -15,8 +15,6 @@ export const createTaskService = async ({
 
   // check if the project exists
   const project = await projects.findById(projectId);
-  console.log(project);
-  console.log(memberId);
 
   if (!project) {
     throw error(404, "Project not found");
@@ -26,6 +24,7 @@ export const createTaskService = async ({
   if (project?.owner.toString() !== userId) {
     throw error(401, "User not authorized for this action");
   }
+
   //   check if the the member trying to create is actually a member in the project
   if (!project?.members?.includes(memberId)) {
     throw error(404, "You are not a member in this project");
@@ -39,4 +38,69 @@ export const createTaskService = async ({
     assignedTo: memberId,
   });
   return task;
+};
+
+// get task service
+export const getTasksService = async ({
+  userId,
+  projectId,
+  page = 1,
+  limit = 10,
+}) => {
+  if (!userId || !projectId) throw error(400, "Missing IDs");
+
+  // skip calculation
+  const skip = (page - 1) * limit;
+
+  // 1. Run queries in parallel for better performance
+  const [availableTasks, total] = await Promise.all([
+    tasks.find({ projectId: projectId }).limit(limit).skip(skip),
+    tasks.countDocuments({ projectId: projectId }),
+  ]);
+
+  return {
+    data: availableTasks,
+    meta: {
+      total,
+      page: Number(page),
+      limit: Number(limit),
+      totalPages: Math.ceil(total / limit),
+    },
+  };
+};
+
+// update task service
+
+export const updateTaskService = async ({
+  userId,
+  taskId,
+  name,
+  description,
+}) => {
+  if (!taskId || !userId) {
+    throw error(404, "Invalid input");
+  }
+
+  const task = await tasks.findOne({ owner: userId, _id: taskId });
+
+  if (!task) {
+    throw error(404, "Task not found");
+  }
+
+  if (task?.owner.toString() !== userId) {
+    throw error(401, "Not authorized for this action");
+  }
+
+  const updatedTask = await tasks.findOneAndUpdate(
+    { owner: userId, _id: taskId },
+    {
+      $set: {
+        name,
+        description,
+      },
+    },
+    { new: true }
+  );
+
+  return updatedTask;
 };
