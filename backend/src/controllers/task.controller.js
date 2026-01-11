c314837343b1f5d1587c83f8857271fcf5e4ec75
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-11 13:52:13 +0100

    Feat: Implement the getTask and edittask in the task controller.

--- backend/src/controllers/task.controller.js
+++ backend/src/controllers/task.controller.js
@@ -1,4 +1,8 @@
-import { createTaskService } from "../service/task.service.js";
+import {
+  createTaskService,
+  getTasksService,
+  updateTaskService,
+} from "../service/task.service.js";
 import { catchAsync } from "../utils/catchAsync.js";
 
 // create task controller
@@ -24,5 +28,42 @@ export const createTask = catchAsync(async (req, res) => {
   res.status(201).json({
     status: "success",
     data: task,
+  });
+});
+
+// get task controller
+export const getTasks = catchAsync(async (req, res) => {
+  const userId = req.user.sub.id;
+  const { projectId } = req.body;
+  const { page, limit } = req.query;
+
+  const tasks = await getTasksService({
+    userId,
+    projectId,
+    page,
+    limit,
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: tasks,
+  });
+});
+
+// update task controller
+export const editTask = catchAsync(async (req, res) => {
+  const { name, description } = req.body;
+  const { taskId } = req.params;
+  const userId = req.user.sub.id;
+
+  const updatedTask = await updateTaskService({
+    userId,
+    taskId,
+    name,
+    description,
+  });
+  res.status(200).json({
+    status: "success",
+    data: updatedTask,
   });
 });
