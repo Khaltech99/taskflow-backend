ebec7bf34d4f807e779c60f201d9b44726013ad3
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-11 13:52:46 +0100

    Add: add the get tas and the edit task route.

--- backend/src/routes/task.route.js
+++ backend/src/routes/task.route.js
@@ -1,9 +1,20 @@
 import Router from "express";
-import { createTask } from "../controllers/task.controller.js";
+import {
+  createTask,
+  editTask,
+  getTasks,
+} from "../controllers/task.controller.js";
 import { protectedRoute } from "../middleware/protectedRoute.js";
 
 const taskRouter = Router();
 
 // create task
 taskRouter.post("/tasks", protectedRoute, createTask);
+
+// get Task
+taskRouter.get("/tasks", protectedRoute, getTasks);
+
+// update Task
+taskRouter.patch("/tasks/:taskId", protectedRoute, editTask);
+
 export default taskRouter;
