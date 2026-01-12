5e631edf3613891aca694cfae35279da085e913a
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-12 15:21:15 +0100

    Implement the winston logger.

--- backend/src/middleware/errorHandler.js
+++ backend/src/middleware/errorHandler.js
@@ -1,5 +1,14 @@
+import { logger } from "../utils/logger.js";
+
 export const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
+  logger.error({
+    message: message,
+    statusCode: statusCode,
+    path: req.path,
+    method: req.method,
+    stack: process.env.NODE_ENV !== "production" ? err.stack : undefined,
+  });
   res.status(statusCode).json({ success: false, message: message });
 };
