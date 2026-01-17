60b8566b258a18398c16104fb2e6d89670c89fac
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-17 17:19:44 +0100

    cleared the next fn called after res  mistakenly.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -6,7 +6,6 @@ export const registerController = catchAsync(async (req, res, next) => {
   const { name, email, password } = req.body;
   const user = await registerService({ name, email, password });
   res.status(201).json({ message: "User registered successfully", user });
-  next();
 });
 
 // login controller
@@ -14,5 +13,4 @@ export const loginController = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
   const user = await loginService({ email, password });
   res.status(200).json({ message: "User logged in successfully", user });
-  next();
 });
