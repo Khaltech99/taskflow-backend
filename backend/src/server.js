c1470b25c8c199ceeb7804e3b6e4b7c251aa0d59
Khaltech99 <olatunjibabatundejunior@gmail.com>
2026-01-17 17:19:01 +0100

    Add fallback port and also connectDb fn.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 import app from "./app.js";
+import { connectDb } from "./config/database.js";
 
-const port = process.env.PORT;
+const port = process.env.PORT || 7300;
+
+connectDb();
 
 app.listen(port, () => {
   console.log(`Running at port:${port}`);
